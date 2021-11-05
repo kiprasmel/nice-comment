412b2c81d6390fbd023d85bc3e2a8feb71b77499
Kipras Melnikovas <kipras@kipras.org>
2021-11-05 20:34:08 +0200

    Initial commit
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

---
+++ README.md
@@ -0,0 +1,13 @@
+# nice-comment
+
+create a nice comment.
+
+<p>
+whenever you feel like using interpolation in template literals (<code>`foo ${bar + 1} baz`</code>),
+<br>
+you could use <code>nice-comment</code> instead.
+</p>
+
+## License
+
+[MIT](./LICENSE)
---
+++ nice-comment.ts
@@ -0,0 +1,36 @@
+type Predicate = (item: string) => string;
+
+export const quote: Predicate = item => `"${item}"`;
+
+export const tick: Predicate = item => "`" + item + "`";
+
+/**
+ * item => `"item"`
+ *
+ * named by the way you would read it in code
+ *
+ * (you probably want this)
+ */
+export const tickQuote: Predicate = item => [item].map(quote).map(tick)[0];
+
+/**
+ * item => "`item`"
+ *
+ * named by the way you would read it in code
+ *
+ * (you probably want the other one)
+ */
+export const quoteTick: Predicate = item => [item].map(tick).map(quote)[0];
+
+export const bracket: Predicate = item => "[" + item + "]";
+
+export const toPrettyArr = (arr: string[] = [], predicate: Predicate = quote, ret: string = ""): string => (
+    (ret = arr.map(predicate).join(", ")), //
+    (ret = bracket(ret)),
+    ret
+);
+
+export const toComment = (comments: (string | string[])[]): string =>
+    comments //
+        .map(line => (Array.isArray(line) ? line.join("") : line))
+        .join("\n\n");
