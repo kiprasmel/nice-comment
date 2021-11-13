faa2f979b641927d87971a5642f350b644b26244
Kipras Melnikovas <kipras@kipras.org>
2021-11-13 04:13:27 +0200

    chore: fix formatting
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- nice-comment.ts
+++ nice-comment.ts
@@ -1,8 +1,8 @@
 type Predicate = (item: string) => string;
 
-export const quote: Predicate = item => `"${item}"`;
+export const quote: Predicate = (item) => `"${item}"`;
 
-export const tick: Predicate = item => "`" + item + "`";
+export const tick: Predicate = (item) => "`" + item + "`";
 
 /**
  * item => `"item"`
@@ -11,7 +11,7 @@ export const tick: Predicate = item => "`" + item + "`";
  *
  * (you probably want this)
  */
-export const tickQuote: Predicate = item => [item].map(quote).map(tick)[0];
+export const tickQuote: Predicate = (item) => [item].map(quote).map(tick)[0];
 
 /**
  * item => "`item`"
@@ -20,17 +20,17 @@ export const tickQuote: Predicate = item => [item].map(quote).map(tick)[0];
  *
  * (you probably want the other one)
  */
-export const quoteTick: Predicate = item => [item].map(tick).map(quote)[0];
+export const quoteTick: Predicate = (item) => [item].map(tick).map(quote)[0];
 
-export const bracket: Predicate = item => "[" + item + "]";
+export const bracket: Predicate = (item) => "[" + item + "]";
 
 export const parens = (prefixInside: string, mid: string, postfixOutside: string = "", midPredicate = tick): string =>
 	"(" + prefixInside + " " + midPredicate(mid) + ")" + postfixOutside;
 
 export const toPrettyArr = (arr: string[] = [], predicate: Predicate = quote, ret: string = ""): string => (
-    (ret = arr.map(predicate).join(", ")), //
-    (ret = bracket(ret)),
-    ret
+	(ret = arr.map(predicate).join(", ")), //
+	(ret = bracket(ret)),
+	ret
 );
 
 export const toComment = (comments: (string | string[] | string[][])[]): string =>
